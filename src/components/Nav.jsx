0fc6b57a5d385c04625a85527e89d9a9693eee25
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2024-01-23 15:37:01 +0100

    add responsive menu and hidden navigation for mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,44 @@
 import NikeLogo from '../assets/nike-logo.svg?react';
 import { RxHamburgerMenu } from "react-icons/rx";
+import { TbShoppingBag } from "react-icons/tb";
+import {useState} from "react";
 
 const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 export function Nav() {
+    const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
     return (
 
         <nav className="flex flex-wrap justify-between items-center">
             <a href="#">
                 <NikeLogo className="h-20 w-20"/>
             </a>
-            <button className="hover:bg-gray-100 p-2 focus:ring-2 focus:ring-gray-200 rounded-lg">
+            <button
+                onClick={() => setIsMobileMenuShown(!isMobileMenuShown)}
+                className="hover:bg-gray-100 p-2 focus:ring-2 focus:ring-gray-200 rounded-lg
+                lg:hidden">
                 <RxHamburgerMenu size={25}/>
             </button>
-            <div className="w-full">
-                <ul className="bg-gray-50 text-lg border border-gray-100 rounded-lg p-4">
+            <div className={`w-full lg:w-auto lg:block 
+            ${isMobileMenuShown === false && "hidden"}`}>
+                <ul className="lg:flex-row flex flex-col bg-gray-50 text-lg border
+                 border-gray-100 rounded-lg p-4 lg:bg-transparent lg:border-none lg:space-x-8">
                     {ROUTES.map((route, i) => {
                        return <li
                            key={route}
                            className={`py-2 px-2 cursor-pointer rounded 
-                           ${i===0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`}
+                           ${i===0 
+                               ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500" 
+                               : "hover:bg-gray-100"}`}
                        >{route}</li>
                     })}
                 </ul>
             </div>
+            <div className="fixed bottom-4 left-4 lg:static">
+                <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+                    <TbShoppingBag/>
+                </div>
+            </div>
+
         </nav>
 
     )
