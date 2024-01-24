1b4dc8add266d3c1be87e3a7452e6d449d10494e
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2024-01-24 16:27:56 +0100

    add CartItem component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {SHOE_LIST} from "./constant.js";
 import {NewArrivalsSection} from "./components/NewArrivalsSection.jsx";
 import {Sidebar} from "./components/Sidebar.jsx";
 import {useState} from "react";
+import {CartItem} from "./components/CartItem.jsx";
 
 export function App() {
 
@@ -16,7 +17,12 @@ export function App() {
         <ShoeDetail/>
         <NewArrivalsSection items={SHOE_LIST}/>
         <Sidebar isOpen={isSidebarOpen} onClickClose={() => setIsSidebarOpen(false)}>
-            Hi
+            <h2 className="text-2xl font-bold mb-10">
+                Cart
+            </h2>
+            <CartItem item={SHOE_LIST[0]}/>
+            <CartItem item={SHOE_LIST[2]}/>
+            <CartItem item={SHOE_LIST[3]}/>
         </Sidebar>
       </div>
     </>
