ace64cee6ff0aa3d1efb61153a96fdcd627a1223
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2024-01-24 14:08:24 +0100

    add Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import {Nav} from "./components/Nav.jsx";
 import {ShoeDetail} from "./components/ShoeDetail.jsx";
+import {Card} from "./components/Card.jsx";
+import {SHOE_LIST} from "./constant.js";
 
 export function App() {
 
   return (
     <>
       <div className="animate-fadeIn p-10 xl:p-24">
-        <Nav/>
-        <ShoeDetail/>
+        {/*<Nav/>*/}
+        {/*<ShoeDetail/>*/}
+          <Card item={SHOE_LIST[0]}/>
       </div>
     </>
   )
